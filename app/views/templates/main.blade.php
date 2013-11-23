68cf32c2e729770a6fcbd9930da7d382d58c3980
Kareem Mohamed <kareem3d.a@gmail.com>
2013-11-23 02:20:33 +0200

    Adding respond js

--- app/views/templates/main.blade.php
+++ app/views/templates/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amer group</title>
 
+    <script src="{{ URL::asset('app/lib/respond.min.js') }}"></script>
+
     {{ $template->printAssets('css') }}
 
 </head>
