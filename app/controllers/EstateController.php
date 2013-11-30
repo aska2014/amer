ccb33e5cb90be0e0c406c8410151847135c7e424
Kareem Mohamed <kareem3d.a@gmail.com>
2013-11-30 11:38:16 +0200

    Showing all estates for non registered users

--- app/controllers/EstateController.php
+++ app/controllers/EstateController.php
@@ -76,8 +76,6 @@ class EstateController extends BaseController {
         $this->images = $images;
         $this->specialPayments = $specialPayments;
         $this->specialOffers = $specialOffers;
-
-        $this->beforeFilter('auth', array('except' => array('all', 'show')));
     }
 
     /**
@@ -119,20 +117,14 @@ class EstateController extends BaseController {
     }
 
     /**
-     * @return mixed
-     */
-    public function create()
-    {
-        return $this->edit($this->estates->newInstance());
-    }
-
-    /**
      * @param \Estate\Estate $estate
      * @throws Exception
      * @return mixed
      */
     public function edit($estate)
     {
+        $this->beforeFilter('auth');
+
         if($estate->exists AND ! Auth::user()->same($estate->user))
         {
             throw new Exception("You can't edit this estate");
@@ -153,6 +145,8 @@ class EstateController extends BaseController {
      */
     public function postEdit(Estate $estate)
     {
+        $this->beforeFilter('auth');
+
         $estate->fill($this->getEstateInputs());
         $estate->ownerInfo->fill($this->getUserInfoInputs());
 
@@ -178,6 +172,89 @@ class EstateController extends BaseController {
 
         // Success redirect to upgrade page with success..
         return Redirect::back()->with('success', trans('messages.success.estate.update'));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function create()
+    {
+        $this->beforeFilter('auth');
+
+        return $this->edit($this->estates->newInstance());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function postCreate()
+    {
+        $this->beforeFilter('auth');
+
+        // New instance without saving to database yet
+        $auction   = $this->newAuction();
+        $estate    = $this->newEstate( $auction != null );
+        $ownerInfo = $this->newOwnerInfo();
+
+        // Save to database
+        if(! $this->validateAndSave($estate, $ownerInfo, $auction))
+        {
+            return Redirect::back()->withErrors($this->errors)->withInput();
+        }
+
+        // Success redirect to upgrade page with success..
+        return Redirect::to(URL::page('estate/upgrade', $estate))->with('success', trans('messages.success.estate.create'));
+    }
+
+    /**
+     * @param \Estate\Estate $estate
+     * @return mixed
+     */
+    public function upgrade( Estate $estate )
+    {
+        $this->beforeFilter('auth');
+
+        $this->throwIfNotOwner($estate);
+
+        $offers = $this->specialOffers->all();
+
+        return $this->page()->printMe(compact('estate', 'offers'));
+    }
+
+    /**
+     * @param \Estate\Estate $estate
+     * @return mixed
+     */
+    public function postUpgrade( Estate $estate )
+    {
+        $this->beforeFilter('auth');
+
+        $this->throwIfNotOwner($estate);
+
+        $specialPayment = $this->specialPayments->newInstance(Input::get('SpecialPayment'));
+
+        $specialPayment->user()->associate(Auth::user());
+
+        $specialPayment->estate()->associate($estate);
+
+        $specialPayment->save();
+
+        return Redirect::to(URL::page('estate/show', $estate))->with('success', trans('messages.success.upgrade'));
+    }
+
+    /**
+     * @param Estate $estate
+     * @throws Exception
+     */
+    public function remove(Estate $estate)
+    {
+        $this->beforeFilter('auth');
+
+        $this->throwIfNotOwner($estate);
+
+        $estate->delete();
+
+        return Redirect::to(URL::page('user/estates'))->with('success', trans('messages.success.estate.delete'));
     }
 
 
@@ -288,51 +365,15 @@ class EstateController extends BaseController {
     }
 
     /**
-     * @return mixed
+     * @param Estate $estate
+     * @throws Exception
      */
-    public function postCreate()
+    protected function throwIfNotOwner(Estate $estate)
     {
-        // New instance without saving to database yet
-        $auction   = $this->newAuction();
-        $estate    = $this->newEstate( $auction != null );
-        $ownerInfo = $this->newOwnerInfo();
-
-        // Save to database
-        if(! $this->validateAndSave($estate, $ownerInfo, $auction))
+        if(! Auth::user()->same($estate->user))
         {
-            return Redirect::back()->withErrors($this->errors)->withInput();
+            throw new Exception("You can't access this page");
         }
-
-        // Success redirect to upgrade page with success..
-        return Redirect::to(URL::page('estate/upgrade', $estate))->with('success', trans('messages.success.estate.create'));
-    }
-
-    /**
-     * @param \Estate\Estate $estate
-     * @return mixed
-     */
-    public function upgrade( Estate $estate )
-    {
-        $offers = $this->specialOffers->all();
-
-        return $this->page()->printMe(compact('estate', 'offers'));
-    }
-
-    /**
-     * @param \Estate\Estate $estate
-     * @return mixed
-     */
-    public function postUpgrade( Estate $estate )
-    {
-        $specialPayment = $this->specialPayments->newInstance(Input::get('SpecialPayment'));
-
-        $specialPayment->user()->associate(Auth::user());
-
-        $specialPayment->estate()->associate($estate);
-
-        $specialPayment->save();
-
-        return Redirect::to(URL::page('estate/show', $estate))->with('success', trans('messages.success.upgrade'));
     }
 
     /**
@@ -400,24 +441,6 @@ class EstateController extends BaseController {
     protected function getAuctionInputs()
     {
         return Input::has('estate-has-auction') ? Input::get('Auction') : array();
-    }
-
-    /**
-     * @param Estate $estate
-     * @throws Exception
-     */
-    public function remove(Estate $estate)
-    {
-        if(! Auth::user()->same($estate->user))
-        {
-            throw new Exception("You can't access this page");
-        }
-        else
-        {
-            $estate->delete();
-
-            return Redirect::to(URL::page('user/estates'))->with('success', trans('messages.success.estate.delete'));
-        }
     }
 
 }
