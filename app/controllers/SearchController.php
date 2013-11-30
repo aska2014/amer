033b43dac0f0d9c21f7ffa4ff44d4bca586706c4
Kareem Mohamed <kareem3d.a@gmail.com>
2013-11-30 16:49:12 +0200

    Fixing search

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -30,7 +30,7 @@ class SearchController extends BaseController {
 
         $this->searchByPrice();
 
-        $estates = $this->estatesAlgorithm->accepted()->paginate(EstateController::ESTATES_PER_PAGE);
+        $estates = $this->estatesAlgorithm->accepted()->orderByDate()->paginate(EstateController::ESTATES_PER_PAGE);
 
         $estatesTitle = 'بحث عن عقار';
 
