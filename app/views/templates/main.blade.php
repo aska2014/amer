1a43d15e4c2ffe4045a4b1bb3a760c35a3b1f8a1
Kareem Mohamed <kareem3d.a@gmail.com>
2014-01-13 19:35:56 +0200

    Fix meta tag

--- app/views/templates/main.blade.php
+++ app/views/templates/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
+    
     @if(isset($seo))
         {{ $seo->toHtml() }}
     @else
